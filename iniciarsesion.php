3ddbe5e7768f60706f0376df76ee9f798bbb7abd
eliasscaceres <elias_cipo@hotmail.com>
2017-04-21 14:55:55 -0300

    add

---
+++ iniciarsesion.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>PHP</title>
+		
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link type="text/css" rel="stylesheet" href="style.css">
+	<?php include ("menuinicio.php"); ?>
+</head>
+
+
+<body>
+	
+</body>
+</html>
